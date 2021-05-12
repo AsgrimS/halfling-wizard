5aa03e41fe20ec2a9e5c9b7de77955c5058ff56c
Asgrim_S <jan.semik@gmail.com>
2021-05-12 20:40:18 +0200

    fonts

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -1,17 +1,42 @@
 import { createGlobalStyle } from "styled-components";
+import PTSansRegular from "./fonts/PTSans-Regular.ttf";
+import PTSansBold from "./fonts/PTSans-Bold.ttf";
+import PTSansItalic from "./fonts/PTSans-Italic.ttf";
+import PTSansBoldItalic from "./fonts/PTSans-BoldItalic.ttf";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
-    background: var(--eerieBlack);
-    color: var(--floralWhite);
-
+  
+  @font-face {
+    font-family: "PT Sans";
+    src: local("PT Sans"), url(${PTSansRegular}) format("truetype");
+  }
+  @font-face {
+    font-family: "PT Sans Bold";
+    src: local("PT Sans Bold"), url(${PTSansBold}) format("truetype");
+  }
+  @font-face {
+    font-family: "PT Sans Italic";
+    src: local("PT Sans Italic"), url(${PTSansItalic}) format("truetype");
+  }
+  @font-face {
+    font-family: "PT Sans Bold-Italic";
+    src: local("PT Sans Bold-Italic"), url(${PTSansBoldItalic}) format("truetype");
+  }
+  
+  *{
     --floralWhite: hsl(46, 100%, 97%);
     --paleSilver: hsl(38, 16%, 76%);
     --blackOlive: hsl(34, 6%, 24%);
     --eerieBlack: hsl(40, 4%, 14%);
     --flame: hsl(17, 83%, 54%);
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background: var(--eerieBlack);
+    color: var(--floralWhite);
+    font-family: "PT Sans";
   }
 `;
 
